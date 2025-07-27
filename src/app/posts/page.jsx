4e7dae357ccb69a06187fe4b6b1869f87e58090f
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-07-27 16:01:28 +0600

    Added metadata in the Services page

--- src/app/posts/page.jsx
+++ src/app/posts/page.jsx
@@ -1,6 +1,11 @@
 import { posts } from "@/lib/posts";
 import Link from "next/link";
 
+// Metadata
+export const metadata = {
+    title: "Posts | My First App",
+    description: "Read more articles or blogs from My first nextjs app"
+}
 const Posts = () => {
     return (
         <div className="grid grid-cols-3 gap-5 max-w-10/12 mx-auto">
